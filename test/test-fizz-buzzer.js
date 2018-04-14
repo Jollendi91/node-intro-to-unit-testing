55a518d8b0a5f19aa590b1bf47084f4c8c4fa50e
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-04-14 17:41:57 -0500

    add not divisible test/refactor

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -5,55 +5,40 @@ const fizzBuzzer = require('../fizzBuzzer');
 describe('fizzBuzzer', function() {
 
     it('should return \'fizz\' if divisible by only 3', function() {
-        const divisBy3Cases = [
-            3,
-            6,
-            9,
-            12
-        ];
+        const divisBy3Cases = [3, 6, 9, 12];
 
         divisBy3Cases.forEach(input => {
-            const answer = fizzBuzzer(input);
-            expect(answer).to.equal('fizz');
+            expect(fizzBuzzer(input)).to.equal('fizz');
         });
 
     });
 
     it('should return \'buzz\' if divisible by only 5', function() {
-        const divisBy5Cases = [
-            5,
-            10,
-            25,
-            55
-        ];
+        const divisBy5Cases = [5, 10, 25, 55];
 
         divisBy5Cases.forEach(input => {
-            const answer = fizzBuzzer(input);
-            expect(answer).to.equal('buzz');
+            expect(fizzBuzzer(input)).to.equal('buzz');
         });
     });
 
     it('should return \'fizz-buzz\' if divisible by 3 and 5', function() {
-        const divisBy3And5Cases = [
-            15,
-            30,
-            45,
-            60
-        ];
+        const divisBy3And5Cases = [15, 30, 45, 60];
 
         divisBy3And5Cases.forEach(input => {
-            const answer = fizzBuzzer(input);
-            expect(answer).to.equal('fizz-buzz');
+            expect(fizzBuzzer(input)).to.equal('fizz-buzz');
         });
     });
 
+    it('should return number if not divisible by 3 or 5', function() {
+        const notDivisibleCases = [2, 4, 7, 11];
+
+        notDivisibleCases.forEach(input => {
+            expect(fizzBuzzer(input)).to.equal(input);
+        });
+    })
+
     it('should raise an error if arg is not a number', function() {
-        const badInputs = [
-            'a',
-            true,
-            '13',
-            false
-        ];
+        const badInputs = ['a', true, '13', false];
 
         badInputs.forEach(input => {
             expect(function() {
